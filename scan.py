a67d4614fd835e31352f1fba4b870bd6b25d5bbc
pouya.fallahi <pouya.fallahi80@gmail.com>
2024-07-26 11:45:41 +0330

    ADD | the code of page 6 of chapter 1

---
+++ scan.py
@@ -0,0 +1,19 @@
+import os
+import argparse
+
+from lxml import etree
+from subprocess import Popen
+
+
+def get_ip(machine_name):
+    pass
+
+
+class Scanner:
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    scan = Scanner()
+    print('Hello!')
